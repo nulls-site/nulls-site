640d07cb48104d13c41525e100fd1dafccb5faa4
NilsonDeon <nilsondeon01@gmail.com>
2026-03-15 20:01:00 -0300

    feature: css, about and animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mandy-red': '#8b0000',
+        'mandy-red-bright': '#c41e3a',
+        'mandy-green': '#2d5016',
+        'mandy-yellow': '#d4a017',
+        'mandy-blue': '#1a4d6e',
+        'mandy-purple': '#3b3154',
+        'mandy-white': '#f5f5f0',
+        'mandy-black': '#0d0d0c',
+      },
+    },
   },
   plugins: [],
 }
